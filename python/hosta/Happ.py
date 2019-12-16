c7ee7358176771e229d5bc90338ec46436682e85
Danil Lykov <lkv97dn@gmail.com>
2019-12-15 22:47:05 -0600

    Refactor

--- python/hosta/Happ.py
+++ python/hosta/Happ.py
@@ -36,56 +36,82 @@ class Happ:
     def _ser_vars(self):
         return json.dumps(self.vars)
 
+## ## ## ## ## ## Listening updates ## ## ## ## ## ## 
+
     async def _handler(self, ws):
         try:
             log.info(f"New connection from {ws.path}")
-            refv = ws.path.split('/')[1]
             log.debug(f"Children {self._child_obj}")
             log.debug(f"Subscribers {self._subscr}")
+
+            refv = ws.path.split('/')[1]
             # if anything is in the path
             if len(refv)>0:
-                # Subscribe this client to monitor vars
-                self._subscr[refv].append( ws )
-                # send the current object to it
-                child = self._child_obj[refv]
-                # if updates to other clients sent, 
-                # Initiate state for current cliet
-                if not child._touched:
-                    yield child.serial()
-
-                while True:
-                    msg = await ws.get_message()
-                    child.inbound = msg
-                yield None
-                await trio.sleep_forever()
+                self._handle_obj_ref(ws, refv)
 
             else:
+                # return link to root element
                 yield ref(self.vars)
         except Exception as e:
             log.error(e)
 
+    async def _handle_obj_ref(self, ws, ref):
+        # Subscribe this client to monitor vars
+        self._subscr[ref].append( ws )
+        # send the current object to it
+        child = self._child_obj[ref]
+        # if updates to other clients sent, 
+        # Initiate state for current cliet
+        if not child._touched:
+            yield child.serial()
+
+        self._child_updating_loop(ws, child)
+
+        yield None
+        # save ws connection open
+        await trio.sleep_forever()
+
+    async def _child_updating_loop(self, ws, child):
+        while True:
+            msg = await ws.get_message()
+            try:
+                updates = json.loads(msg)
+                child.update( updates )
+            except json.JSONDecodeError:
+                log.error("JSON decode error: {}",msg)
+
+## ## ## ## ## ## Serving updates ## ## ## ## ## ## 
+
+    def _get_alive_listeners(self, ref):
+        listeners  = self._subscr[ref]
+        return [ ws for ws in listeners if not ws.closed]
+
+    async def _send_child_to_listeners(self, child, listeners):
+        message = child.serial()
+        try:
+            for ws in listeners:
+                await ws.send_message(message)
+            child._mark_untouched()
+        except Exception as e:
+            log.error("Error sending update to {}: {}", ws, e)
+
     async def _monitor_vars(self):
         while True:
             for r in self._child_obj:
-                listeners  = self._subscr[r]
-                alive = [ws for ws in listeners if not ws.closed]
+                alive = self._get_alive_listeners(r)
                 self._subscr[r] = alive
                 listeners = alive
                 if len(listeners) > 0:
                     child = self._child_obj[r]
                     if child._touched:
-                        message = child.serial()
-                        try:
-                            for ws in listeners:
-                                await ws.send_message(message)
-                            child._mark_untouched()
-                        except Exception as e:
-                            log.error("Error sending update to {}: {}", ws, e)
+                        await self._send_child_to_listeners(child, listeners)
 
             if not self._running:
                 self._cancel_scope.cancel()
 
             await trio.sleep(.02)
+
+## ## ## ## ## ## Starting ## ## ## ## ## ## 
 
     async def _start(self):
         with self._cancel_scope:
@@ -102,6 +128,9 @@ class Happ:
         t = thr.Process(target=trio.run, args=(self._start,))
         t.start()
         return t
+
+## ## ## ## ## ## Stopping ## ## ## ## ## ## 
+
     def stop(self):
         self._running = False
 
