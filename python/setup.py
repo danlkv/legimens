4f43e7187c7bf0bfeec681529f2aa45224628e87
Danil Lykov <lkv97dn@gmail.com>
2020-01-12 19:46:25 -0600

    python 3.6

--- python/setup.py
+++ python/setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='legimens',
-    version='0.1',
+    version='0.1.1',
     license='MIT',
 
     packages=find_packages(),
-    python_requires='>=3.7',
+    python_requires='>=3.6',
 
     install_requires = ['loguru'],
     setup_requires = ['pytest-runner'],
