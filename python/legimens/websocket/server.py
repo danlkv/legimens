e825291063790150a90e63866d6a0792c1805bdf
Danil Lykov <lkv97dn@gmail.com>
2020-11-23 02:21:53 -0600

    try to add port retries

--- python/legimens/websocket/server.py
+++ python/legimens/websocket/server.py
@@ -2,6 +2,8 @@ import trio
 from trio_websocket import serve_websocket, ConnectionClosed
 from loguru import logger as log
 
+PORT_RETRIES = 5 #handle port in use: not used
+
 class StopServer(Exception):
     pass
 
@@ -19,14 +21,32 @@ async def ws_serve(addr, port, iterable_fn, nursery=None):
         except ConnectionClosed as e:
             log.warning(f"Connection closed by remote: {e}")
             return
-    try:
-        await serve_websocket(server, addr, port,
-                              ssl_context=None,
-                              handler_nursery=nursery)
-    except OSError as ose:
-        log.error(f"Websocket start on {port} failed: {ose}")
-        raise
-        return
+
+    for i in range(PORT_RETRIES+1):  #handle port in use: not used
+        try:
+            await serve_websocket(server, addr, port,
+                                  ssl_context=None,
+                                  handler_nursery=nursery)
+            break
+        except OSError as ose:
+            #
+            # Handling port in use produces a problem when you don't know where to connect in front.
+            # Let's better fail.
+            #
+            log.error(f"Websocket start on {port} failed: {ose}")
+            raise
+
+            #   if ose.args[0] != 98:
+            #       log.error(f"Websocket start on {port} failed: {ose}")
+            #       raise
+            # --#handle port in use: not used
+            log.warning(f"Port {port} is already in use")
+            if i == PORT_RETRIES:
+                log.error(f"Failed to start websocket after {PORT_RETRIES} attempts to find an open port.")
+                raise
+            log.info(f"Trying to start websocket on {addr}:{port+1}...")
+            await ws_serve(addr, port+1, iterable_fn, nursery=nursery)
+            # --#handle port in use: not used
     log.info("Websocket terminates")
 
 async def start_server(addr, port, handler_func=print, nursery=None):
